f9646bc185c541ab9f30d5dfb3b005ff422e93f5
Nathan Smith <nathan@praisetopia.org>
2018-03-27 15:49:12 -0500

    First pass at dice roller. Needswork.

--- src/public/js/dice-roller.js
+++ src/public/js/dice-roller.js
@@ -1,3 +1,14 @@
-function rollDie(sides) {
-    return Math.floor((Math.random() * sides) + 1);
+const diceButton = document.querySelector('#dicebtn');
+const diceInput = document.querySelector('#diceinput');
+// const addRollsToLogCheck = document.querySelector('#addRollsToLog');
+
+function rollDice() {
+    // TODO: Validate input
+    const rolls = d20.roll(diceInput.value, true);
+    const total = rolls.reduce((prev, current) => prev + current);
+    console.log(total);
+    // TODO: Show output
+    // TODO: Add result to log
 }
+
+diceButton.addEventListener('click', rollDice);
